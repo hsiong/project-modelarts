fba2faecb71ee9c472b0b3995e7c1d20983c9079
hsiong <peak1203@126.com>
2024-04-10 10:30:12 +0800

    init commit

--- huawei/sign/main.py
+++ huawei/sign/main.py
@@ -2,14 +2,15 @@
 import os
 import requests
 from apig_sdk import signer
+from config import config
 
 if __name__ == '__main__':
     sig = signer.Signer()
     # Set the AK/SK to sign and authenticate the request.
     # 认证用的ak和sk硬编码到代码中或者明文存储都有很大的安全风险，建议在配置文件或者环境变量中密文存放，使用时解密，确保安全；
     # 本示例以ak和sk保存在环境变量中为例，运行本示例前请先在本地环境中设置环境变量HUAWEICLOUD_SDK_AK和HUAWEICLOUD_SDK_SK。
-    sig.Key = os.getenv('HUAWEICLOUD_SDK_AK')
-    sig.Secret = os.getenv('HUAWEICLOUD_SDK_SK')
+    sig.Key = os.getenv(config.SK)
+    sig.Secret = os.getenv(config.AK)
 
     # The following example shows how to set the request URL and parameters to query a VPC list.
     # Set request Endpoint.
